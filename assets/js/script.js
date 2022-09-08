c4488b79a416a1bf474f92df84b55c75a323f48d
briancollins02 <brianmcollins0@gmail.com>
2022-09-07 21:03:50 -0400

    adds active listener to enable touchstart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,22 @@
 console.log("I'm connected!");
 
+// Enables touchstart for all elements, including ones not visible on load
+document.addEventListener("touchstart", function() {}, false); 
 
+// Define Checkbox
 const checkbox = document.querySelector(".toggler");
 
-
-
-
-function openMenu() {
-    if(checkbox.checked) {
-        console.log("the checkbox is checked!");
-    }
-    else {
-        console.log("the checkbox is NOT checked!")
-    }
-}
- openMenu(); 
-
-
+// Tracks active element
  const navClick = document.activeElement;
 
- console.log(navClick.activeElement);
-
-
+// Console log active element when clicked
  document.addEventListener('focusin', function() {
     console.log('focused: ', document.activeElement)
  }, true);
 
+// If menu open and nav is clicked, close menu
  document.addEventListener('focusin', function() {
     if (navClick.activeElement = document.getElementsByClassName('nav-buttons')) {
         checkbox.checked  = false;
-        console.log('switcheroo')
     }
 });
